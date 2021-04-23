428b4b053a117ef292333f050d73f8efba35094f
bala-wellcare <balaji.varadharajan@wellcare.com>
2021-04-23 13:04:35 -0400

    Initial Add

---
+++ src/main/java/com/example/demo/web/controllers/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Balaji Varadharajan
+ * Date: 4/23/2021
+ * Time: 1:02 PM
+ * To change this template use File | Settings | File and Code Templates.
+ */
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+
+    @GetMapping
+    public String demo(){
+        return "Hello !!!";
+    }
+}
